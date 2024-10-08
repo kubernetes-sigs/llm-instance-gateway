4fe234e9f89ba0be1960ac2047685d9000be3932
Cong Liu <conliu@google.com>
2024-10-08 16:57:49 +0000

    Improve the filter to return multiple preferred pods instead of one; also fix metric update bug
    Returning multiple preferred pods improves the probability to match the
    next preferrence filter.
    
    Also updated log levels:
    - Level 1, 2: For non-spammy logs like initialization
    - Level 3+: For per request debug logs
    - Level 4+: For per loop logs like periodically scrape metrics

--- pkg/ext-proc/scheduling/filter.go
+++ pkg/ext-proc/scheduling/filter.go
@@ -43,16 +43,16 @@ func (f *filter) Name() string {
 
 func (f *filter) Filter(b *LLMRequest, pods []*backend.PodMetrics) ([]*backend.PodMetrics, error) {
 	if f == nil {
-		klog.V(2).Infof("Running nil filter, returning all input pods by default")
+		klog.V(3).Infof("Running nil filter, returning all input pods by default")
 		return pods, nil
 	}
-	klog.V(2).Infof("Running filter %q on request %v with %v pods", f.name, b, len(pods))
+	klog.V(3).Infof("Running filter %q on request %v with %v pods", f.name, b, len(pods))
 
 	filtered, err := f.filter(b, pods)
 
 	next := f.nextOnSuccessOrFailure
 	if err == nil {
-		klog.V(2).Infof("onSuccess %v -> %v, filtered: %v", f.name, next.Name(), len(filtered))
+		klog.V(3).Infof("onSuccess %v -> %v, filtered: %v", f.name, next.Name(), len(filtered))
 		if f.nextOnSuccess != nil {
 			next = f.nextOnSuccess
 		}
@@ -60,7 +60,7 @@ func (f *filter) Filter(b *LLMRequest, pods []*backend.PodMetrics) ([]*backend.P
 		return next.Filter(b, filtered)
 	}
 
-	klog.V(2).Infof("onFailure %v -> %v", f.name, next.Name())
+	klog.V(3).Infof("onFailure %v -> %v", f.name, next.Name())
 	if f.nextOnFailure != nil {
 		next = f.nextOnFailure
 	}
@@ -88,32 +88,57 @@ func toFilterFunc(pp podPredicate) filterFunc {
 	}
 }
 
+// leastQueuingFilterFunc finds the max and min queue size of all pods, divides the whole range
+// (max-min) by the number of pods, and finds the pods that fall into the first range.
+// The intuition is that if there are multiple pods that share similar queue size in the low range,
+// we should consider them all instead of the absolute minimum one. This worked better than picking
+// the least one as it gives more choices for the next filter, which on aggregate gave better
+// results.
+// TODO: Compare this strategy with other strategies such as top K.
 func leastQueuingFilterFunc(b *LLMRequest, pods []*backend.PodMetrics) ([]*backend.PodMetrics, error) {
 	min := math.MaxInt
+	max := 0
 	filtered := []*backend.PodMetrics{}
+
 	for _, pod := range pods {
-		if pod.WaitingQueueSize < min {
+		if pod.WaitingQueueSize <= min {
 			min = pod.WaitingQueueSize
-			filtered = []*backend.PodMetrics{}
 		}
-		if pod.WaitingQueueSize == min {
+		if pod.WaitingQueueSize >= max {
+			max = pod.WaitingQueueSize
+		}
+	}
+
+	for _, pod := range pods {
+		if pod.WaitingQueueSize >= min && pod.WaitingQueueSize <= min+(max-min)/len(pods) {
 			filtered = append(filtered, pod)
 		}
 	}
 	return filtered, nil
 }
 
+// leastKVCacheFilterFunc finds the max and min KV cache of all pods, divides the whole range
+// (max-min) by the number of pods, and finds the pods that fall into the first range.
+// The intuition is that if there are multiple pods that share similar KV cache in the low range, we
+// should consider them all instead of the absolute minimum one. This worked better than picking the
+// least one as it gives more choices for the next filter, which on aggregate gave better results.
+// TODO: Compare this strategy with other strategies such as top K.
 func leastKVCacheFilterFunc(b *LLMRequest, pods []*backend.PodMetrics) ([]*backend.PodMetrics, error) {
-	min := math.MaxInt
+	min := math.MaxFloat64
+	max := math.SmallestNonzeroFloat64
 	filtered := []*backend.PodMetrics{}
-	margin := 5
+
 	for _, pod := range pods {
-		cur := int(pod.KVCacheUsagePercent) / margin
-		if cur < min {
-			min = cur
-			filtered = []*backend.PodMetrics{}
+		if pod.KVCacheUsagePercent <= min {
+			min = pod.KVCacheUsagePercent
 		}
-		if cur == min {
+		if pod.KVCacheUsagePercent >= max {
+			max = pod.KVCacheUsagePercent
+		}
+	}
+
+	for _, pod := range pods {
+		if pod.KVCacheUsagePercent >= min && pod.KVCacheUsagePercent <= min+(max-min)/float64(len(pods)) {
 			filtered = append(filtered, pod)
 		}
 	}
