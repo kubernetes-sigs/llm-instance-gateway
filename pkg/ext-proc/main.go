fe7cfb2eada3bd45fe3a2bafd30ee0246183e734
kaushikmitr <kaushikmitra.umd@gmail.com>
2024-10-25 18:00:45 +0000

    add TLS policy and timeouts, updated ext proc to read certs

--- pkg/ext-proc/main.go
+++ pkg/ext-proc/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 	"os"
-	"os/signal"
 	"strings"
-	"syscall"
 	"time"
 
 	extProcPb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 	healthPb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/status"
 	klog "k8s.io/klog/v2"
@@ -31,6 +34,7 @@ var (
 
 	refreshPodsInterval    = flag.Duration("refreshPodsInterval", 10*time.Second, "interval to refresh pods")
 	refreshMetricsInterval = flag.Duration("refreshMetricsInterval", 50*time.Millisecond, "interval to refresh metrics")
+	certPath               = ""
 )
 
 type healthServer struct{}
@@ -44,8 +48,33 @@ func (s *healthServer) Watch(in *healthPb.HealthCheckRequest, srv healthPb.Healt
 	return status.Error(codes.Unimplemented, "Watch is not implemented")
 }
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	certPool, err := loadCA(certPath)
+	if err != nil {
+		log.Fatalf("Could not load CA certificate: %v", err)
+	}
+
+	// Create TLS configuration
+	tlsConfig := &tls.Config{
+		RootCAs:    certPool,
+		ServerName: "instance-gateway-ext-proc.envoygateway",
+	}
+
+	// Create gRPC dial options
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)),
+	}
+
+	conn, err := grpc.Dial("localhost:9002", opts...)
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	_ = conn
+}
+
 func main() {
 	klog.InitFlags(nil)
+	flag.StringVar(&certPath, "certPath", "", "path to extProcServer certificate and private key")
 	flag.Parse()
 
 	// This is the list of addresses of backend pods.
@@ -71,7 +100,12 @@ func main() {
 		klog.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	creds, err := loadTLSCredentials(certPath)
+	if err != nil {
+		log.Fatalf("Failed to load TLS credentials: %v", err)
+	}
+
+	s := grpc.NewServer(grpc.Creds(creds))
 
 	pp := backend.NewProvider(&vllm.PodMetricsClientImpl{}, &backend.FakePodLister{Pods: pods})
 	if err := pp.Init(*refreshPodsInterval, *refreshMetricsInterval); err != nil {
@@ -82,16 +116,57 @@ func main() {
 
 	klog.Infof("Starting gRPC server on port :%v", *port)
 
-	// shutdown
-	var gracefulStop = make(chan os.Signal)
-	signal.Notify(gracefulStop, syscall.SIGTERM)
-	signal.Notify(gracefulStop, syscall.SIGINT)
 	go func() {
-		sig := <-gracefulStop
-		klog.Infof("caught sig: %+v", sig)
-		os.Exit(0)
+		err = s.Serve(lis)
+		if err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
 	}()
 
-	s.Serve(lis)
+	http.HandleFunc("/healthz", healthCheckHandler)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 
+}
+
+func loadTLSCredentials(certPath string) (credentials.TransportCredentials, error) {
+	// Load extProcServer's certificate and private key
+	crt := "server.crt"
+	key := "server.key"
+
+	if certPath != "" {
+		if !strings.HasSuffix(certPath, "/") {
+			certPath = fmt.Sprintf("%s/", certPath)
+		}
+		crt = fmt.Sprintf("%s%s", certPath, crt)
+		key = fmt.Sprintf("%s%s", certPath, key)
+	}
+	certificate, err := tls.LoadX509KeyPair(crt, key)
+	if err != nil {
+		return nil, fmt.Errorf("could not load extProcServer key pair: %s", err)
+	}
+
+	// Create a new credentials object
+	creds := credentials.NewTLS(&tls.Config{Certificates: []tls.Certificate{certificate}})
+
+	return creds, nil
+}
+
+func loadCA(caPath string) (*x509.CertPool, error) {
+	ca := x509.NewCertPool()
+	caCertPath := "server.crt"
+	if caPath != "" {
+		if !strings.HasSuffix(caPath, "/") {
+			caPath = fmt.Sprintf("%s/", caPath)
+		}
+		caCertPath = fmt.Sprintf("%s%s", caPath, caCertPath)
+	}
+	caCert, err := os.ReadFile(caCertPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read ca certificate: %s", err)
+	}
+	ca.AppendCertsFromPEM(caCert)
+	return ca, nil
 }
